b65ca282ea6f12db9307ad10171bb92060762891
kerengoldd <kerengold55@gmail.com>
2017-07-15 20:34:26 +0300

    /

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -13,6 +14,7 @@ import {HttpModule} from "@angular/http";
 import { ComplainDetailsComponent } from './home/complain-details/complain-details.component';
 import { ComplainListComponent } from './home/complain-list/complain-list.component';
 import { ComplainItemComponent } from './home/complain-list/complain-item/complain-item.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -29,9 +31,13 @@ import { ComplainItemComponent } from './home/complain-list/complain-item/compla
   imports: [
     BrowserModule,
     AppRouting,
-    HttpModule
+    HttpModule,
+      FormsModule
   ],
-  providers: [ComplainService],
+
+  providers: [ComplainService ,
+    {provide: LocationStrategy,
+      useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
